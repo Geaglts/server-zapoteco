376a63eaafcf8c0f0f024565efb3991fc0768995
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2020-12-08 17:10:46 -0600

    Codificacion del controlador de set rols

---
+++ src/controllers/Usuario.Controller.js
@@ -0,0 +1,55 @@
+import { PrismaClient } from "@Prisma/client";
+
+const prisma = new PrismaClient();
+
+const validLength = (field) => {
+    if (field.length > 0) {
+        return true;
+    }
+    return false;
+};
+
+export default {
+    rols: {
+        async set(parent, { rolids, usuarioid }, context) {
+            try {
+                //Valida los datos de entrada
+                const validInputs =
+                    validLength(rolids) && validLength(usuarioid);
+                if (!validInputs) {
+                    return { status: false };
+                }
+
+                //Elimina todos los roles del usuario
+                await prisma.usuario_rol.deleteMany({
+                    //where id_usuario = usuarioid
+                    where: {
+                        usuarioid,
+                    },
+                });
+
+                for (const rolid of rols) {
+                    //Asigna el rol
+                    await prisma.usuario_rol.create({
+                        //connect where id usuario = usuarioid
+                        //connect where id rol = rolid
+                        data: {
+                            rol: {
+                                connect: {
+                                    id: rolid,
+                                },
+                            },
+                            usuario: {
+                                connect: {
+                                    id: usuarioid,
+                                },
+                            },
+                        },
+                    });
+                }
+            } catch (e) {
+                throw new Error(e);
+            }
+        },
+    },
+};
