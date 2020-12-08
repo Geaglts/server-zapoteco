2013967d84ad9bbe926bd32901ef8a2dbec8b19c
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2020-12-08 07:37:26 -0600

    Codificacion terminada de los controladores de tipos

--- src/controllers/Tipos.js
+++ src/controllers/Tipos.js
@@ -5,6 +5,12 @@ export default {
     async create(parent, { tipo }, context) {
         try {
             // Nuevo tipo
+            const nuevoTipo = await prisma.tipos.create({
+                data: { tipo },
+            });
+
+            console.log(nuevoTipo);
+
             return { status: true, msg: "Creado correctamente" };
         } catch (err) {
             throw new Error(err);
@@ -22,6 +28,13 @@ export default {
     async update(parent, { id, tipo }, context) {
         try {
             // Actualizar un tipo
+            const actualizacion = await prisma.tipos.update({
+                where: { id },
+                data: { tipo },
+            });
+
+            console.log(actualizacion);
+
             return { status: true, msg: "Actualizado correctamente" };
         } catch (err) {
             throw new Error(err);
